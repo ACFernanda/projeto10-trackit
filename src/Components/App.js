18e33eb4ab2967b5f2858783fdbfb46704e510ad
acfernanda <acfernanda98@gmail.com>
2022-03-28 21:57:08 -0300

    login page

---
+++ src/Components/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
+import HabitsPage from "./HabitsPage";
+import TodayPage from "./TodayPage";
+import HistoryPage from "./HistoryPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/cadastro" element={<RegisterPage />}></Route>
+        <Route path="/habitos" element={<HabitsPage />}></Route>
+        <Route path="/hoje" element={<TodayPage />}></Route>
+        <Route path="/historico" element={<HistoryPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/Components/LoginPage.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import logo from "../assets/img/Logo.svg";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function login() {
+    const promise = axios.post(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login",
+      {
+        email: email,
+        password: password,
+      }
+    );
+  }
+
+  return (
+    <Container>
+      <img src={logo} alt="TrackIt" />
+
+      <form onSubmit={login}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+      </form>
+
+      <Link to="/cadastro">
+        <p>Não tem uma conta? Cadastre-se!</p>
+      </Link>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  font-family: "Lexend Deca", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 75vh;
+
+  img {
+    width: 60%;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  input {
+    width: 100%;
+    height: 45px;
+    background: #ffffff;
+    border: 1px solid #d5d5d5;
+    box-sizing: border-box;
+    border-radius: 5px;
+    font-size: 19.976px;
+    line-height: 25px;
+    color: #dbdbdb;
+    margin-bottom: 6px;
+  }
+
+  button {
+    width: 100%;
+    height: 45px;
+    background: #52b6ff;
+    border: none;
+    border-radius: 4.63636px;
+    font-size: 20.976px;
+    line-height: 26px;
+    text-align: center;
+    color: #ffffff;
+  }
+
+  p {
+    font-size: 13.976px;
+    line-height: 17px;
+    text-align: center;
+    text-decoration-line: underline;
+    color: #52b6ff;
+    margin-top: 25px;
+  }
+`;
